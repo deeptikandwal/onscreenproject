08ccd32e6df000cb8eeec55175d4eb8512cd2e5c
LL\deekandw <deekandw@publicisgroupe.net>
2022-12-08 12:32:08 +0530

    test case for usecases..

--- app/src/test/java/com/project/onscreen/com/project/onscreen/data/usecase/GetAnimeUseCaseImplTest.kt
+++ app/src/test/java/com/project/onscreen/com/project/onscreen/data/usecase/GetAnimeUseCaseImplTest.kt
@@ -1,4 +1,73 @@
 package com.project.onscreen.com.project.onscreen.data.usecase
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.project.onscreen.data.api.ApiHelper
+import com.project.onscreen.data.model.Anime
+import com.project.onscreen.data.model.Employee
+import com.project.onscreen.data.repository.MainRepository
+import com.project.onscreen.data.usecase.GetAnimesUseCaseImpl
+import com.project.onscreen.data.usecase.GetEmployeesUseCase
+import com.project.onscreen.data.usecase.GetEmployeesUseCaseImpl
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TestRule
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+@OptIn(ExperimentalCoroutinesApi::class)
+
 class GetAnimeUseCaseImplTest {
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+    val dispatcher = TestCoroutineDispatcher()
+    lateinit var getAnimesUseCaseImpl: GetAnimesUseCaseImpl
+    @Mock
+    lateinit var apiHelper: ApiHelper
+    @Mock
+    lateinit var mainRepository: MainRepository
+    @Mock
+    lateinit var mockList:ArrayList<Anime>
+    @Before
+    fun setUp(){
+        MockitoAnnotations.initMocks(this)
+        Dispatchers.setMain(dispatcher)
+        getAnimesUseCaseImpl= GetAnimesUseCaseImpl(apiHelper,mainRepository)
+    }
+
+    @Test
+    fun `get Employees when repository is empty`()= runTest{
+        val arrayList= arrayListOf(Anime())
+        Mockito.`when`(mainRepository.getAnimeList()).thenReturn(mockList)
+        Mockito.`when`(mockList.isEmpty()).thenReturn(true)
+        Mockito.`when`(mainRepository.getQuery()).thenReturn("bleach")
+        Mockito.`when`(apiHelper.getAnimeQuotes("Naruto")).thenReturn(arrayList)
+        getAnimesUseCaseImpl.getAnimes("Naruto")
+        Mockito.verify(mainRepository).saveAnimes(arrayList)
+        Mockito.verify(mainRepository).clearAnimes()
+        Mockito.verify(mainRepository,Mockito.times(1)).getAnimeList()
+        Mockito.verify(mainRepository,Mockito.times(1)).saveQuery("Naruto")
+    }
+
+    @Test
+    fun `get Employees when repository is not empty`()= runTest{
+        Mockito.`when`(mainRepository.getAnimeList()).thenReturn(mockList)
+        Mockito.`when`(mockList.isEmpty()).thenReturn(false)
+        Mockito.`when`(mainRepository.getQuery()).thenReturn("Naruto")
+        getAnimesUseCaseImpl.getAnimes("Naruto")
+        Mockito.verify(mainRepository,Mockito.times(2)).getAnimeList()
+    }
+    @After
+    fun tearDown(){
+        Dispatchers.resetMain()
+    }
 }
--- app/src/test/java/com/project/onscreen/com/project/onscreen/data/usecase/GetEmployeeUseCaseImpl.kt
+++ app/src/test/java/com/project/onscreen/com/project/onscreen/data/usecase/GetEmployeeUseCaseImpl.kt
@@ -2,6 +2,7 @@ package com.project.onscreen.com.project.onscreen.data.usecase
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.project.onscreen.data.api.ApiHelper
+import com.project.onscreen.data.model.Employee
 import com.project.onscreen.data.repository.MainRepository
 import com.project.onscreen.data.usecase.GetEmployeesUseCase
 import com.project.onscreen.data.usecase.GetEmployeesUseCaseImpl
@@ -9,13 +10,16 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 import org.mockito.Mock
+import org.mockito.Mockito
 import org.mockito.MockitoAnnotations
 
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -29,6 +33,8 @@ class GetEmployeeUseCaseImpl {
     lateinit var apiHelper: ApiHelper
     @Mock
     lateinit var mainRepository: MainRepository
+    @Mock
+    lateinit var mockList:ArrayList<Employee>
     @Before
     fun setUp(){
         MockitoAnnotations.initMocks(this)
@@ -37,8 +43,22 @@ class GetEmployeeUseCaseImpl {
     }
 
     @Test
-    fun getEmployeesSuccessTest(){
+    fun `get Employees when repository is empty`()= runTest{
+        val arrayList= arrayListOf(Employee(1,"Jonas","jona@gmail.com"))
+        Mockito.`when`(mainRepository.getEmployeesList()).thenReturn(mockList)
+        Mockito.`when`(mockList.isEmpty()).thenReturn(true)
+        Mockito.`when`(apiHelper.getEmployees()).thenReturn(arrayList)
+        getEmployeesUseCaseImpl.getEmployees()
+        Mockito.verify(mainRepository).saveEmployees(arrayList)
+        Mockito.verify(mainRepository,Mockito.times(1)).getEmployeesList()
+    }
 
+    @Test
+    fun `get Employees when repository is not empty`()= runTest{
+        Mockito.`when`(mainRepository.getEmployeesList()).thenReturn(mockList)
+        Mockito.`when`(mockList.isEmpty()).thenReturn(false)
+        getEmployeesUseCaseImpl.getEmployees()
+        Mockito.verify(mainRepository,Mockito.times(2)).getEmployeesList()
     }
     @After
     fun tearDown(){
